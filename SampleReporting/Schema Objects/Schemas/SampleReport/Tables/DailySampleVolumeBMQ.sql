8fb57532f474fe1ea8facd2fd4329239ef4bbaca
BenKing2608 <ben.king@ipsos.com>
2023-01-31 14:54:01 +0000

    BK TEST V9

--- SampleReporting/Schema Objects/Schemas/SampleReport/Tables/DailySampleVolumeBMQ.sql
+++ SampleReporting/Schema Objects/Schemas/SampleReport/Tables/DailySampleVolumeBMQ.sql
@@ -3,7 +3,7 @@
 	[Market] [nvarchar](255) NULL,
 	[Brand] [nvarchar](255) NULL,
 	[Questionnaire] [nvarchar](255) NULL,
-	[Frequency] [nvarchar](255) NULL,
+	[Frequency] [nvarchar](200) NULL,
 	[File_Count] [int] NULL,
 	[FileRow_Count] [bigint] NULL,
 	[FileRow_LoadedCount] [int] NULL,
