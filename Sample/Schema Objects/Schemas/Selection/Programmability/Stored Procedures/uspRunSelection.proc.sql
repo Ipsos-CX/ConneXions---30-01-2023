33694c91ce93e6a527d93b372a255579b58fab0f
BenKing2608 <ben.king@ipsos.com>
2023-03-17 15:17:44 +0000

    TASK 1234 - CHECKED OUT

--- Sample/Schema Objects/Schemas/Selection/Programmability/Stored Procedures/uspRunSelection.proc.sql
+++ Sample/Schema Objects/Schemas/Selection/Programmability/Stored Procedures/uspRunSelection.proc.sql
@@ -3,9 +3,6 @@ CREATE PROCEDURE [Selection].[uspRunSelection]
 	@SelectionRequirementID [dbo].[RequirementID]
 AS
 /*
-
-STATUS: CHECKED OUT – BK <2023-03-17> OR CHECKED IN – BK <2023-03-17>
-
 		Purpose: main sample selection run
 		
 		Version			Date			Developer			Comment
--- Sample/Schema Objects/Schemas/dbo/Tables/Ben_Test.sql
+++ Sample/Schema Objects/Schemas/dbo/Tables/Ben_Test.sql
@@ -1,13 +1,4 @@
 ﻿
-/*
-STATUS: CHECKED OUT – BK <2023-03-17> OR CHECKED IN – BK <2023-03-17>
-
-              Version                    Date                 Developer            Comment
-UAT           1.1                        2023-02-21           Chris Ledger         Task 831 - add  q_jlr_service_preferred_option_yn field
-
-*/
-
-
 CREATE TABLE [dbo].[10053_BenTest](
 	[SampleDealerPartyID] [int] NULL,
 	[DealerPartyID] [int] NULL,
